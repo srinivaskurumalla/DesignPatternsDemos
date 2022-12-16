72173c4fcbb4dd9cecdd791d2a89c7b900e50ec0
srinivaskur <srinivaskur@ICS-LAP-7099>
2022-12-16 14:27:19 +0530

    Add project files.

---
+++ DesignPatternsDemos.SingletonDemo/CabService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsDemos.SingletonDemo
+{
+    public class CabService
+    {
+        private List<string> cabDrivers = new List<string>();
+        private int count = 0;
+
+        private CabService()
+        {
+            cabDrivers.AddRange(new string[] { "Rajini", "Kamal", "Vijay", "Ajith" });
+        }
+
+        private static CabService cabService = new CabService();
+        public static CabService GetDriver()
+        {
+            return cabService;
+        }
+
+        public string GetNextAvailableDriver()
+        {
+            string result = cabDrivers[count];
+            count++;
+            if (count >= cabDrivers.Count)
+            {
+                count = 0;
+            }
+            return result;
+        }
+    }
+
+}
---
+++ DesignPatternsDemos.SingletonDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsDemos.SingletonDemo
+{
+    class Program
+    {
+        static CabService Customer1cabService = CabService.GetDriver(); // new CabService();
+        static CabService Customer2cabService = CabService.GetDriver(); //new CabService();
+        public static void Customer1NextDriver()
+        {
+            Console.WriteLine($"Next Available Driver for Customer 1 :{Customer1cabService.GetNextAvailableDriver()}");
+        }
+
+        public static void Customer2NextDriver()
+        {
+            Console.WriteLine($"Next Available Driver for Customer 2 :{Customer2cabService.GetNextAvailableDriver()}");
+        }
+
+        static void Main(string[] args)
+        {
+
+            CabService cabService = CabService.GetDriver();
+            //Console.WriteLine($"Next Available Driver :{cabService.GetNextAvailableDriver()}");
+
+            for (int i = 0; i < 4; i++)
+            {
+                // Console.WriteLine($"Next Available Driver :{cabService.GetNextAvailableDriver()}");
+                Customer1NextDriver();
+                Customer2NextDriver();
+            }
+        }
+    }
+
+}
